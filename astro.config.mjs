27e9bdf2cde65069dca65c56471085eaf49453c0
Donovan Childers <donovan@smartz.com>
2023-03-07 16:44:04 -0800

    Add astro image package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
 const site = process.env.CI_ENVIRONMENT_URL ?? 'http://localhost:3000'; // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 // @ts-check
@@ -9,8 +10,10 @@ const site = process.env.CI_ENVIRONMENT_URL ?? 'http://localhost:3000'; // Full 
 // https://astro.build/config
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: site,
   trailingSlash: 'always',
-  integrations: [sitemap(), mdx()]
+  integrations: [sitemap(), mdx(), image()]
 });
