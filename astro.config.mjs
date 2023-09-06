49e05e2f53bce57784f9157aa7dde1a48f688f75
Donovan Childers <donovan@smartz.com>
2023-09-06 03:22:51 +0000

    Upgrade to Astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
 const site = process.env.CI_ENVIRONMENT_URL ?? 'http://localhost:3000'; // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 // @ts-check
@@ -15,5 +14,5 @@ const site = process.env.CI_ENVIRONMENT_URL ?? 'http://localhost:3000'; // Full 
 export default defineConfig({
   site: site,
   trailingSlash: 'always',
-  integrations: [sitemap(), mdx(), image()]
+  integrations: [sitemap(), mdx()]
 });
